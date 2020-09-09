ac48eaf0470898ca94ea9e7e426c1366d97d166a
katrina <kmrjelinek@gmail.com>
2020-09-09 11:31:33 -0600

    adds four new products

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,13 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+# product = Product.new(name: "Banana", price: 0.50, image_path: , description: "a ripe banana")
+
+product2 = Product.new(name: "Tide Pods", price: 18, image_path: , description: "laundry detergent vessel")
+
+product3 = Product.new(name: "Xbox", price: 100, image_path: , description: "home gaming system")
+
+product4 = Product.new(name: "Hair Brush", price: 2, image_path: , description: "hair detangling tool")
+
+product5 = Product.new(name: "Dog Toy", price: 4, image_path: , description: "durable squeaky toy for dogs")
