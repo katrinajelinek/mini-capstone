c80f246f72ecb6f4205623c55b6ffdffa64c90d0
katrinajelinek <kmrjelinek@gmail.com>
2020-09-30 13:14:27 -0600

    adds index and new actions

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -3,4 +3,25 @@ class ProductsController < ApplicationController
     @products = Product.all
     render "index.html.erb"
   end
+
+  def new
+    render "new.html.erb"
+  end
+
+  def create
+    product = Product.new(
+      { name: params[:name],
+        price: params[:price],
+        description: params[:description],
+        inventory: params[:inventory],
+        supplier_id: params[:supplier_id] }
+    )
+    product.save
+    redirect_to "/products/#{product.id}"
+  end
+
+  def show
+    @product = Product.find(params[:id])
+    render "show.html.erb"
+  end
 end
