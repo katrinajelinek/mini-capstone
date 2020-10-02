824450ccef528ba0c9db5ca10079768278b3b2a8
katrinajelinek <kmrjelinek@gmail.com>
2020-10-02 08:19:11 -0600

    adds html code to products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -24,4 +24,28 @@ class ProductsController < ApplicationController
     @product = Product.find(params[:id])
     render "show.html.erb"
   end
+
+  def edit
+    @product = Product.find(params[:id])
+    render "edit.html.erb"
+  end
+
+  def update
+    @product = Product.find(params[:id])
+    @product.update(
+      { name: params[:name],
+        price: params[:price],
+        description: params[:description],
+        inventory: params[:inventory],
+        supplier_id: params[:supplier_id] }
+    )
+    @product.save
+    redirect_to "/products/#{product.id}"
+  end
+
+  def destroy
+    @product = Product.find(params[:id])
+    @product.destroy
+    redirect_to "/products"
+  end
 end
